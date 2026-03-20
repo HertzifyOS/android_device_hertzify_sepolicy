LineageOS/android_device_lineage_sepolicy

atv: Update from newest DroidLogic devkit
* Move existing public/private sepolicy to system_ext, as
  that's where these apps live.
* `BOARD_SEPOLICY_DIRS` -> `BOARD_VENDOR_SEPOLICY_DIRS`.

Change-Id: If8877d899fdadb2dc516db86dfe1df95a3015ae5

atv/sepolicy.mk
@@ -4,9 +4,12 @@
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-BOARD_SEPOLICY_DIRS += \
+BOARD_VENDOR_SEPOLICY_DIRS += \
     device/lineage/sepolicy/atv/vendor
 endif
 
-PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+   device/lineage/sepolicy/atv/system_ext/public
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+   device/lineage/sepolicy/atv/system_ext/private

HertzifyOS/android_device_hertzify_sepolicy

Signed-off-by: HDzungx <hdzungx@gmail.com>

atv/sepolicy.mk
@@ -10,3 +10,6 @@ endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
    device/hertzify/sepolicy/atv/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+   device/hertzify/sepolicy/atv/system_ext/public